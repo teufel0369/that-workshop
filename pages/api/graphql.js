c935a537020fbe6f4673722d29e38bef0d8069f4
Chris Thompson <cthompson@armsbusinesssolutions.com>
2019-08-05 14:17:22 -0500

    setting up graphql

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,13 +1,43 @@
 const { ApolloServer, gql } = require("apollo-server-micro");
 
+// must have a query type
 const schema = gql`
-  type Query {
+  type Speaker {
+    id: ID!
+    firstname: String!
+    lastname: String!
+    title: String!
+    company: String!
+    biography: String!
+  }
+  
+   type Query {
     aboutMessage: String
+    speakers: [Speaker!]!
   }
 `;
 
 const resolvers = {
   Query: {
+    aboutMessage: () => "THAT Conference was founded by Clark Sell",
+    speakers: () => [
+        {
+          id: 1,
+          firstname: "Matt",
+          lastname: "Reetz",
+          title: "Software Engineer",
+          company: "Headway",
+          biography: "Matt is a coding ninja."
+        },
+        {
+          id: 2,
+          firstname: "Tim",
+          lastname: "Gremore",
+          title: "Software Engineer",
+          company: "Headway",
+          biography: "Tim is a coding wizard."
+        }
+    ]
   }
 };
 
